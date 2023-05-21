0ca5dd31b355204d1b4a211218bb7b31d6ff332b
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-05-21 10:29:23 -0700

    Starter code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,6 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        dribbble: "#EA4C89",
-        instagram: "#E4405F",
-        twitter: "#1DA1F2",
-        linkedin: "#0A66C2",
-      },
-    },
-  },
   plugins: [],
   darkMode: "class",
 };
